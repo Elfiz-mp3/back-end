77214988095710b7fac3e5876d39fc480ce8f97e
Elfiz-mp3 <muhammadalfathirnugraha@gmail.com>
2025-12-08 13:35:22 +0700

    Add files via upload
    Tugas pertemuan 4 TofuCode

---
+++ Pertemuan 3/t2_asosiatif_array.php
@@ -0,0 +1,15 @@
+<?php
+$souls = [
+    "wife" => "Ranni the Witch",
+    "he/them" => "Dark Sun Gwyndolin",
+    "maiden" => "Melina",
+    "boss" => "Malenia, Blade of Miquella",
+    "maiden2" => "Fire Keeper",
+];
+echo "Wife: " . $souls["wife"] . PHP_EOL;
+echo "Yes: " . $souls["he/them"] . PHP_EOL;
+echo "Elden Maiden: " . $souls["maiden"] . PHP_EOL;
+echo "Rot Goddess: " . $souls["boss"] . PHP_EOL;
+echo "DS3 Maiden: " . $souls["maiden2"] . PHP_EOL;
+
+?>
