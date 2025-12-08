2cb73fdc6d2c7261e6f672e5e707ebaf567e637b
Elfiz-mp3 <muhammadalfathirnugraha@gmail.com>
2025-12-08 13:44:50 +0700

    Delete Pertemuan 3 directory

--- Pertemuan 3/t1_array_foreach.php
+++
@@ -1,7 +0,0 @@
-<?php
-$souls = ["Ranni the Witch", "Dark Sun Gwyndolin", "Melina", "Melina, Blade of Miquella", "Fire Keeper"];
-
-foreach ($souls as $waifu) {
-    echo $waifu . PHP_EOL;
-}
-?>
--- Pertemuan 3/t2_sorting.php
+++
@@ -1,9 +0,0 @@
-<?php
-$souls = ["Ranni the Witch", "Dark Sun Gwyndolin", "Melina", "Malenia, Blade of Miquella", "Fire Keeper"];
-
-sort($souls);
-echo "Setelah sorting: ";
-print_r($souls);
-echo PHP_EOL;
-
-?>
